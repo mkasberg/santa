13b5b916a080ef8f3bba6360dc9fa12dbb45aa4c
Mike Kasberg <kasberg.mike@gmail.com>
2018-12-08 16:40:43 -0700

    Spring Boot Initializr Source

---
+++ src/main/java/com/mikekasberg/santa/SantaApplication.java
@@ -0,0 +1,12 @@
+package com.mikekasberg.santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaApplication.class, args);
+	}
+}
