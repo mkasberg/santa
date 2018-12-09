fbba3ca0d4db9d39d8442eedc706d31a2dba7b61
Mike Kasberg <kasberg.mike@gmail.com>
2018-12-08 18:18:13 -0700

    Just making the list better

--- src/main/java/com/mikekasberg/santa/model/Person.java
+++ src/main/java/com/mikekasberg/santa/model/Person.java
@@ -1,17 +1,40 @@
 package com.mikekasberg.santa.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Person {
 
     private String name;
     private int age;
+
+    public static PersonBuilder builder() {
+        return new PersonBuilder();
+    }
+
+    public static class PersonBuilder {
+        private String name;
+        private int age;
+
+        private PersonBuilder() {}
+
+        public PersonBuilder name(String name) {
+            this.name = "The Grinch";
+            return this;
+        }
+
+        public PersonBuilder age(int age) {
+            this.age = 53;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(name, age);
+        }
+    }
 
 }
